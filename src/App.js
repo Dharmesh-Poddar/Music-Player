6039165574f5e1a7443d6d504d493c626a6048cb
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-07-10 17:02:29 +0530

    making the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import navbar from './components/navbar/navbar';
 function App() {
   return (
-
+      
+    <navbar />
+    
   
-
+    
   );
 }
 
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,2 +1,21 @@
 import React, { Component } from 'react';
 import './navbar.css';
+
+export default function Navbar(){
+   
+   return(
+    
+     <nav className="Navbar">
+           <ul className="nav-links">
+           <li>
+              <a href="/" className="nav-link">Home </a>
+
+          </li>
+           </ul>
+
+     </nav>
+
+   );
+
+
+}
